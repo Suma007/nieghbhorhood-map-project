78606eff46cc018e71e80f5cb93d68d28fb58287
Suma007 <sumaprakash2012@gmail.com>
2016-12-24 12:21:18 +0530

    commit 2

--- js/main.js
+++ js/main.js
@@ -2,44 +2,61 @@
 var model = function (){
 	location= [
 		{
-			lat: 8.5583,
-			lng: 76.8812,
+			pos:{lat: 8.5583,
+			lng: 76.8812},
 			title: 'Technopark'
 		},
 		{
-			lat: 8.4004,
-			lng: 76.9787,
+			pos:{lat: 8.4004,
+			lng: 76.9787},
 			title: 'Kovalam'
 		},
 		{
-			lat: 8.4784498,
-			lng: 76.911906,
+			pos:{lat: 8.4784498,
+			lng: 76.911906},
 			title:'Shanghumugam'
 		},
 		{
-			lat: 8.5028,
-			lng: 76.9513,
+			pos:{lat: 8.5028,
+			lng: 76.9513},
 			title: 'Palayam'
 		},
 		{
-			lat: 8.4909,
-			lng: 76.9527,
+			pos:{lat: 8.4909,
+			lng: 76.9527},
 			title: 'Thampanoor'
 		},
 		{
-			lat: 8.5086,
-			lng: 76.9657,
+			pos:{lat: 8.5086,
+			lng: 76.9657},
 			title: 'Vellayambalam'
 		}],
 	loc= ['Technopark','Palayam','Thampanoor','Vellayambalam','Kovalam','Shanghumugam']
 };
 var viewModel = function() {
 	var self = this;
-	var marker=ko.oberservableArray([]);
-	var locations=ko.oberservableArray([]);
-	var mo= model();
+	var marker=ko.observableArray([]);
+	var locations=ko.observableArray([]);
+	var mo= new model();
 	locations.push(mo.location);
-	
+	console.log(locations);
+	var i=0;
+	function addmarker() {
+		// body...
+		for( i=0;i<locations.length;i++){
+			var pos=locations[i].pos;
+			var title= locations[i].title;
+		var m= new google.maps.Marker({
+			position: pos,
+			map: map,
+			title: title,
+			animation: google.maps.animation.DROP,
+			id: i
+		});
+		marker.push(m);
+	}
+	}
+
 
 }
-ko.applyBindings(new model());
+ko.applyBindings(new viewModel());
